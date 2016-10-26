51d549324a2527214b1152c1bdeb59315ff19afc
Thales Ferreira <thales.ferreira@ryerson.ca>
2016-10-26 07:49:59 +0000

    adding plebe level

---
+++ plebe/java/IOFile/FileToText.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.io.*;
+
+public class FileToText{
+
+     public static void main(String []args){
+        
+        try{
+            String text = new Scanner( new File("textfile.txt") ).useDelimiter("\\A").next();
+            System.out.println(text);
+        } catch (FileNotFoundException e) {System.err.println("IndexOutOfBoundsException: " + e.getMessage());} 
+    
+    }
+}
