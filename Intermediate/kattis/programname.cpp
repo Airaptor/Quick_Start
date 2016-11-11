e9a39ce42765cf1d86580f24395107ee4335e608
Thales Ferreira <thales.ferreira@ryerson.ca>
2016-11-11 01:06:51 -0500

    Create programname.cpp

---
+++ Intermediate/kattis/programname.cpp
@@ -0,0 +1,23 @@
+#include<iostream>
+using namespace std;
+
+/*
+/ Fuction to get the first integer as the input limit
+/ Implementation in main:
+/ ------------------------------------------------
+/  int firstIntDelimOfInt = firstIntDelimOfInt();
+/ ------------------------------------------------
+*/
+int firstIntDelimOfInt()
+{
+   int numLim;
+   cin >> numLim;
+   return numLim;
+}
+
+int main()
+{
+   int res = 0;
+   cout << res;
+   return 0;
+}
